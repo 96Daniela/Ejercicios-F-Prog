a9bb0267377768026999d3396f0e07b03c3dedae
Erika <Erika@DESKTOP-Q1B2A49>
2016-10-30 19:34:42 -0500

    Codigo cambiado

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Ejercicios;
+
 
 /**
  *
@@ -11,4 +11,39 @@ package Ejercicios;
  */
 public class Cuenta {
     
+    private double saldo; // variable de instancia que almacena el saldo
+
+   // constructor  
+   public Cuenta( double saldoInicial )
+   {
+      // valida que saldoInicial sea mayor que 0.0; 
+      // si no lo es, saldo se inicializa con el valor predeterminado 0.0
+      if ( saldoInicial > 0.0 ) 
+         saldo = saldoInicial; 
+   } // fin del constructor de Cuenta
+
+   // abona (suma) un monto a la cuenta
+   public void abonar( double monto )
+   {      
+      saldo = saldo + monto; // suma el monto al saldo 
+   } // fin del método abonar
+
+   // devuelve el saldo de la cuenta
+   public double obtenerSaldo()
+   {
+      return saldo; // proporciona el valor de saldo al método que hizo la llamada
+   } // fin del método obtenerSaldo
+   
+   public String retiro(double dinero){
+       String cadena;
+      if(dinero > saldo){
+          saldo= saldo-dinero;
+          cadena = "Retiro exitoso";    
+      }else{
+          saldo=saldo;
+          cadena= "Fallo retiro";
+            
+      }
+      return cadena;
+   }
 }
--- src/PruebaCuenta.java
+++ src/PruebaCuenta.java
@@ -1,9 +1,11 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Ejercicios;
 
 /**
  *
@@ -11,4 +13,60 @@ package Ejercicios;
  */
 public class PruebaCuenta {
     
+    public static void main( String[] args ) 
+   {
+      Cuenta cuenta1 = new Cuenta( 50.00 ); // crea objeto Cuenta
+      Cuenta cuenta2 = new Cuenta( -7.53 ); // crea objeto Cuenta
+    
+      // muestra el saldo inicial de cada objeto
+      System.out.printf( "Saldo de cuenta1: $%.2f\n", 
+         cuenta1.obtenerSaldo() );
+      System.out.printf( "Saldo de cuenta2: $%.2f\n\n", 
+         cuenta2.obtenerSaldo() );
+      
+      // crea objeto Scanner para obtener la entrada de la ventana de comandos
+      Scanner entrada = new Scanner( System.in );
+      double montoDeposito; // deposita el monto escrito por el usuario
+
+      System.out.print( "Escriba el monto a depositar para cuenta1: " ); // indicador
+      montoDeposito = entrada.nextDouble(); // obtiene entrada del usuario
+      System.out.printf( "\nsumando %.2f al saldo de cuenta1\n\n", 
+         montoDeposito );
+      cuenta1.abonar( montoDeposito ); // suma al saldo de cuenta1
+
+      // muestra los saldos
+      System.out.printf( "Saldo de cuenta1: $%.2f\n", 
+         cuenta1.obtenerSaldo() );
+      System.out.printf( "Saldo de cuenta2: $%.2f\n\n", 
+         cuenta2.obtenerSaldo() );
+
+      System.out.print( "Escriba el monto a depositar para cuenta2: " ); // indicador
+      montoDeposito = entrada.nextDouble(); // obtiene entrada del usuario
+      System.out.printf( "\nsumando %.2f al saldo de cuenta2\n\n", 
+         montoDeposito );
+      cuenta2.abonar( montoDeposito ); // suma al saldo de cuenta2
+
+      // muestra los saldos
+      System.out.printf( "Saldo de cuenta1: $%.2f\n", 
+         cuenta1.obtenerSaldo() );
+      System.out.printf( "Saldo de cuenta2: $%.2f\n", 
+         cuenta2.obtenerSaldo() );
+      System.out.print( "Escriba el monto a depositar para cuenta2: " ); // indicador
+      montoDeposito = entrada.nextDouble(); // obtiene entrada del usuario
+      System.out.printf( "\nsumando %.2f al saldo de cuenta2\n\n", 
+         montoDeposito );
+      cuenta2.abonar( montoDeposito ); // suma al saldo de cuenta2
+
+       System.out.println("");
+      //retira saldos
+       System.out.print( "Escriba el monto a retirar para cuenta2: " ); // indicador
+      double retiroSaldo = entrada.nextDouble(); // obtiene entrada del usuario
+      System.out.printf( "\nrestando %.2f al saldo de cuenta2\n\n", 
+         retiroSaldo);
+      System.out.println(cuenta2.retiro(retiroSaldo));
+      // muestra los saldos
+      System.out.printf( "--Saldo de cuenta2: $%.2f\n\n", 
+         cuenta2.obtenerSaldo() );
+      
+   } // fin de main
 }
